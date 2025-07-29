83a93761305dd9fe87d117c72e65262b1062dc2c
Nora <gdv.nora@gmail.com>
2025-07-29 13:52:22 +0200

    Added menu toggling

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,32 @@
 // Grab elements
+const selectElement = selector => {
+    const element = document.querySelector(selector)
+    if(element) return element;
+    throw new Error(`huh? Make sure ${selector} exists or is typed correctly`)
+};
 
 //Nav styles on scroll
+const scrollHeader = () => {
+    const headerElement = selectElement('#header');
+    /* Check if scroll has gone past certain point */
+    if (this.scrollY >= 15 ){
+        headerElement.classList.add('activated'); /* Adds class activated to headerElement */
+    }else{
+        headerElement.classList.remove('activated');
+    }
+}
 
+window.addEventListener('scroll', scrollHeader) /* Listens for event scroll. When triggered calls scrollHeader */
 // Open menu & search pop-up
 
+const menuToggleIcon = selectElement('#menu-toggle-icon');
+const toggleMenu = () => {
+    const mobileMenu = selectElement('#menu');
+    mobileMenu.classList.toggle('activated');
+    menuToggleIcon.classList.toggle('activated');
+};
+
+menuToggleIcon.addEventListener('click', toggleMenu);
 // Open/Close search form popup
 
 // -- Close the search form popup on ESC keypress
